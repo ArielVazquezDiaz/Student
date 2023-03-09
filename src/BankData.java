d92e950d762be643eed75450f1b59478098f8cdc
Ariel Vazquez <22670036@itiguala.tecnm.mx>
2023-03-09 07:39:17 -0600

    Actualizacion del codigo

--- src/BankData.java
+++ src/BankData.java
@@ -1,39 +1,53 @@
+import java.io.IOException;
 import java.util.Scanner;
 public class BankData {
-    public static void main(String[] args) {
+    public static int menu(){
         Scanner sc = new Scanner(System.in);
-        int opc,ban;
+        int opc;
+        System.out.println("¿Que desea realizar en su cuenta?");
+        System.out.println("[1] Saldo");
+        System.out.println("[2] Deportar");
+        System.out.println("[3] Retirar");
+        System.out.println("[0] Salir");
+        opc = sc.nextInt();
+        return opc;
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int opc=0,ban;
         double newBalance;
         double balance;
         bankingElements primeraCuenta = new bankingElements(
                 "Pedro",
-                45000.67,
+                4000,
                 123456789,
                 "Banamex");
-        System.out.println("¿Que desea realizar en su cuenta?");
-        System.out.println("1) depositar o 2) retirar");
-        opc=sc.nextInt();
-        System.out.println("Ingrese la catidad");
-        ban=sc.nextInt();
 
-        if(opc==1){
-            System.out.println("Ingrese el valor a depositar");
-            newBalance= (double) primeraCuenta.getCurrentbalance()+ban;
-            primeraCuenta.setCurrentbalance(newBalance);
-        }
-        else if(opc==2){
-            System.out.println("Ingrese el valor a retirar");
-            newBalance= (double) primeraCuenta.getCurrentbalance()-ban;
-            primeraCuenta.setCurrentbalance(newBalance);
-        }
+        do{
+            opc=menu();
+            if(opc==1){
+                System.out.println("Su saldo es :"+primeraCuenta.getCurrentbalance());
+            }
+            if(opc==2){
+                System.out.println("Ingrese la catidad");
+                ban=sc.nextInt();
+                System.out.println("Ingrese el valor a depositar");
+                newBalance= (double) primeraCuenta.getCurrentbalance()+ban;
+                primeraCuenta.setCurrentbalance(newBalance);
+                System.out.println("Su saldo actual es: "+ newBalance);
+            }
+            if(opc==3){
+                System.out.println("Ingrese la catidad");
+                ban=sc.nextInt();
+                System.out.println("Ingrese el valor a retirar");
+                newBalance= (double) primeraCuenta.getCurrentbalance()-ban;
+                primeraCuenta.setCurrentbalance(newBalance);
+                System.out.println(" Su saldo es de:" + newBalance);
+            }
+        }while (opc!=0);
+        System.out.println("La opcion seleccionada es: "+ opc);
+
         primeraCuenta.toString();
-
-        bankingElements segundaCuenta = new bankingElements();
-        System.out.println("El nombre es:"+segundaCuenta.getOwnerName());
-        System.out.println("El saldo actual es:"+segundaCuenta.getCurrentbalance());
-        System.out.println("La clave intervancaria es:"+segundaCuenta.getInterbanKey());
-        System.out.println("Eñ banco es:"+segundaCuenta.getBankName());
-        segundaCuenta.toString();
     }
 }
 
